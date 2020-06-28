a2b50e6f2193ce6d4b78148b12ffa655f6ada5c5
DanMesk <67519332+DanMesk@users.noreply.github.com>
2020-06-28 15:03:49 -0700

    Create GuessingGame.java

---
+++ src/GuessingGame.java
@@ -0,0 +1,31 @@
+public class GuessingGame
+{
+    public static void mai(String [] args)
+    {
+        Intro();
+        Game();
+        PlayAgain();
+        Results();
+    }
+
+    // intro for the game and how the game is played
+    public static void Intro() {
+
+    }
+
+    // the method for the game (main code)
+    public static void Game() {
+
+    }
+
+    // this method prompts the user if they want to play again
+    public static void PlayAgain() {
+
+    }
+
+    // this method will display the results of the game played
+    public static void Results() {
+
+    }
+    
+}

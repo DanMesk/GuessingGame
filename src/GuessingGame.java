08fc660e69dd9b901fc7f82aeed64fed5d21489d
DanMesk <67519332+DanMesk@users.noreply.github.com>
2020-07-03 17:06:30 -0700

    Final Code

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,16 +1,13 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessingGame
+public class DMGuessingGame
 {
-    // The types that allow for the game to function and other variables to be called for
-    public static int numGen;
-    public static int userGuess;
-    public static double totalGames;
-    public static double numberOfGuesses;
-    public static int bestMin = Integer.MAX_VALUE;
-
-    public static Scanner input = new Scanner(System.in);
+    private static double totalGames;
+    private static double numberOfGuesses;
+    private static int bestNum = Integer.MAX_VALUE;
+    // global user input to use for all methods
+    private static final Scanner input = new Scanner(System.in);
 
     // The methods for the program
     public static void main(String[] args)
@@ -19,7 +16,7 @@ public class GuessingGame
         Game();
     }
     // intro for the game and how the game is played
-    public static void gameIntro()
+    private static void gameIntro()
     {
         System.out.print("This program allows you to play a guessing game.");
         System.out.printf("%nI will think of a number between 1 and");
@@ -30,48 +27,46 @@ public class GuessingGame
     }
 
     // the method for the game (main code)
-    public static void Game()
+    private static void Game()
     {
         // counts how many games played
         totalGames++;
 
         Random rand = new Random();
-        numGen = rand.nextInt(5);
-        boolean Play = false;
+        int numGen = rand.nextInt(5);
 
         // counts the number of tries
         int numTries = 0;
 
         // The core part of the game
+        int userGuess;
         do {
             System.out.print("Guess a number between 1 and 5: ");
             userGuess = input.nextInt();
             numTries++;
+            numberOfGuesses++;
 
-            if (userGuess == numGen)
-            {
-                Play = true;
-
-            } else if (userGuess > numGen)
+            if (userGuess > numGen)
             {
                 System.out.println("Its lower");
             } else {
                 System.out.println("Its higher");
             }
-            numberOfGuesses++;
 
-        } while (!Play);
+        } while (userGuess != numGen);
 
-        if (numberOfGuesses < bestMin)
+        if (numTries < bestNum)
         {
-            bestMin = (int) numberOfGuesses;
+            bestNum = numTries;
         }
+
         System.out.println("You win! It took you " + numTries + " tries");
         // calls the PlayAgain method
         PlayAgain();
     }
+
     // this method prompts the user if they want to play again
-    public static void PlayAgain()
+    private static void PlayAgain()
     {
         String answer;
         System.out.print("Would you like to play again y/n? ");
@@ -93,11 +88,11 @@ public class GuessingGame
     }
 
     // this method will display the results of the game played
-    public static void Results()
+    private static void Results()
     {
         System.out.println("Total games played: " + totalGames);
         System.out.println("Total guesses: " + numberOfGuesses);
         System.out.printf("Average guesses per game: %,.2f%n", numberOfGuesses/totalGames);
-        System.out.println("Best game: " + bestMin);
+        System.out.print("Best game: " + bestNum);
     }
 }
